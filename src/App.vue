a51eeb6d52a108a6d6a41038b3d9a47ce6fb58a8
PC <dijanic@gmail.com>
2025-01-05 21:38:05 +0100

    * Added logic for checking validity for adding classes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello world {{ name }}</h1>
+
+  <p v-if="professor">User is a professor</p>
+  <p v-else>User is not a professor</p>
+
+  <p v-if="age >= 18">User is 18+</p>
+  <p v-else>User is not 18</p>
+
+  <ol>
+    <li v-on:click="deleteClass(singleClass)" v-for="singleClass in classes" :key="singleClass">{{ singleClass }}</li>
+  </ol>
+
+  <form @submit.prevent>
+    <input v-model="name" type="text" placeholder="Unesite vase ime" />
+  </form>
+
+  <form @submit.prevent>
+    <input ref="classInputName" v-on:keyup="checkClassNameValidity" type="text" placeholder="Unesite ime kursa" />
+    <input :disabled="disabledClassButton" v-on:click="addClass" type="button" value="Dodajte kurs" />
+  </form>
+
+  <button v-on:click="changeUserType">Promeni tipa korisnika</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      name: "Toma",
+      professor: false,
+      age: 11,
+      classes: ['Javascript', 'VueJS', 'ReactJS', 'NodeJS'],
+      disabledClassButton: true,
+    }
+  },
+  methods: {
+    changeUserType() {
+      this.professor = !this.professor;
+    },
+
+    deleteClass(name) {
+      const index = this.classes.indexOf(name); // 0, 1, 2, 3
+      this.classes.splice(index, 1);
+    },
+
+    addClass() {
+      const className = this.$refs.classInputName.value;
+      this.classes.push(className);
+    },
+
+    checkClassNameValidity() {
+
+      const className = this.$refs.classInputName.value;
+      const courses = this.classes.map(val => val.toLowerCase());
+      const courseName = className.toLowerCase();
+
+      if(className.trim() === "") {
+        this.disabledClassButton = true;
+      } else this.disabledClassButton = courses.includes(courseName); // courses.includes(courseName); -> True / False
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
